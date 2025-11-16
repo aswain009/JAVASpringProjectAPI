7bb0555787d68b9c11158fd50bb556f19d34d8fb
adamswain <avswain91@gmail.com>
2025-11-16 18:54:04 -0500

    adding login handlers

--- src/main/java/com/example/lugnutsautomotive/repository/AdminRepository.java
+++ src/main/java/com/example/lugnutsautomotive/repository/AdminRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-    Optional<Admin> findByUsername(String user_name);
+    Optional<Admin> findByUser_name(String user_name);
 }
